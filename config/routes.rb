2f4d17482be17d8e722d943d4fac55f98738f3fc
Mitia Blaise <mitia.blaise@gmail.com>
2025-11-24 16:53:06 +0100

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :users do
+    resources :restaurants
+    resources :chats do
+      resources :messages
+    end
+  end
 end
