d5e56c3733145341682a3c20c42ca031c23a1a00
Lucas Renard <lucas.renard83@gmail.com>
2025-11-25 15:01:24 +0100

    added stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,8 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
-  def home
-  end
 
-  def dashboard
+  def home
+    # @chat = current_user.chat.find()
     @user = current_user
     @restaurants = @user.restaurants.where(params[:id])
   end
