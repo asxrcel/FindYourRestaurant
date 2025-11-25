f7b5982ddf64576e7543c34786ae94864ade40d4
Martin Cassan <martin.cassan@gmail.com>
2025-11-25 12:07:22 +0100

    log in & out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "/dashboard", to: "pages#dashboard"
 
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :users do
-    resources :restaurants
+  resources :users, only: [:show, :new, :create, :delete] do
+    resources :restaurants, only: [:index, :show, :delete]
     resources :chats do
       resources :messages
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,16 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+puts "generating 9 restaurants"
 
-Restaurant.new(name: "Indian Food", adress: "61 rue de servan, 75011", budget: "20-40€", category: "indian", rating: 3.8, localisation: "Paris")
+Restaurant.new(user_id: 1, name: "Indian Food", address: "61 rue de servan, 75011", budget: "20-40€", category: "indian", rating: 3.8)
+Restaurant.new(user_id: 1, name: "Chinese Food", address: "45 rue de servan, 75011", budget: "15-35€", category: "chinese", rating: 4.8)
+Restaurant.new(user_id: 1, name: "African Food", address: "12 rue de servan, 75011", budget: "10-20€", category: "african", rating: 3.8)
+Restaurant.new(user_id: 1, name: "Master Poulet", address: "89 rue d'Oberkampf, 75011", budget: "10-15€", category: "fast-food", rating: 4.2 )
+Restaurant.new(user_id: 1, name: "Good Meal", address: "61 rue de Republique, 75010", budget: "10-15€", category: "fast-food", rating: 4.3)
+Restaurant.new(user_id: 1, name: "French Food", address: "90 rue de servan, 75011", budget: "20-40€", category: "french", rating: 4.8)
+Restaurant.new(user_id: 1, name: "Thaï Land", address: "61 rue de Oberkampf, 75011", budget: "10-30€", category: "thaï", rating: 5.0)
+Restaurant.new(user_id: 1, name: "Biryani Land", address: "96 rue de servan, 75011", budget: "20-40€", category: "indian", rating: 4.8 )
+Restaurant.new(user_id: 1, name: "NemNem", address: "40 Xuanwumenwai Dajie Beijing 100052", budget: "6-15$", category: "indian", rating: 4.1)
 
-Restaurant.new(name: "Chinese Food", adress: "45 rue de servan, 75011", budget: "15-35€", category: "chinese", rating: 4.8, localisation: "Paris")
-
-Restaurant.new(name: "African Food", adress: "12 rue de servan, 75011", budget: "10-20€", category: "african", rating: 3.8, localisation: "Paris")
-
-Restaurant.new(name: "Master Poulet", adress: "89 rue d'Oberkampf, 75011", budget: "10-15€", category: "fast-food", rating: 4.2 , localisation: "Paris")
-
-Restaurant.new(name: "Good Meal", adress: "61 rue de Republique, 75010", budget: "10-15€", category: "fast-food", rating: 4.3, localisation: "Paris")
-
-Restaurant.new(name: "French Food", adress: "90 rue de servan, 75011", budget: "20-40€", category: "french", rating: 4.8, localisation: "Paris")
-
-Restaurant.new(name: "Thaï Land", adress: "61 rue de Oberkampf, 75011", budget: "10-30€", category: "thaï", rating: 5.0, localisation: "Paris")
-
-Restaurant.new(name: "Burger Food", adress: "69 rue de servan, 75011", budget: "20-40€", category: "american", rating: 4.0, localisation: "Paris")
-
-Restaurant.new(name: "Biryani Land: "96 rue de servan, 75011", budget: "20-40€", category: "indian", rating: 4.8 , localisation: "Paris")
+puts "generation done"
