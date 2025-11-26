c561aa8a84610ef2780702507780e6cd8ee311ad
Mitia Blaise <mitia.blaise@gmail.com>
2025-11-26 14:57:20 +0100

    Added favorites

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def home
-    @restaurants = current_user.restaurants.last(5)
+    @restaurants = current_user.restaurants.where(favorite: true)
   end
 
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,10 @@
 class RestaurantsController < ApplicationController
+  def update
+    @chat = current_user.chats.find(params[:chat_id])
+    selected_ids = params[:restaurant_ids] || []
+    @chat.restaurants.each do |restaurant|
+      restaurant.update(favorite: selected_ids.include?(restaurant.id.to_s))
+    end
+    redirect_to root_path
+  end
 end
